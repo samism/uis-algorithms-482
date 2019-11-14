ff2069b5358b23692b9fc31e9abbc9f81e4401e8
Sameer Ismail <sam@samism.io>
2019-11-13 22:20:58 -0600

    fix jest / tsc config so that breakpoints work

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
+const { defaults: tsjPreset } = require('ts-jest/presets');
+
 module.exports = {
     roots: ['<rootDir>/src'],
+    verbose: true,
+    moduleFileExtensions: ['ts', 'js'],
     transform: {
-        '^.+\\.tsx?$': 'ts-jest',
+        ...tsjPreset.transform,
     },
+    testMatch: ['**/*.spec.(ts|js)'],
 };

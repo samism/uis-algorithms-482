b8bb4385529b284844ee1e2a3cfbf156110bde63
Sameer Ismail <sam@samism.io>
2019-11-17 17:52:51 -0600

    Update to have UUIDs

--- src/GraphEdge.ts
+++ src/GraphEdge.ts
@@ -1,14 +1,17 @@
 import { GraphVertex } from './GraphVertex';
+import { v4 as uuid } from 'uuid';
 
 export class GraphEdge {
     private readonly _weight: number;
     private readonly _origin: GraphVertex;
     private readonly _destination: GraphVertex;
+    private readonly _uuid: string;
 
     constructor(origin: string, destination: string, weight: number) {
         this._origin = new GraphVertex(origin);
         this._destination = new GraphVertex(destination);
         this._weight = weight;
+        this._uuid = uuid();
     }
 
     get weight(): number {
@@ -21,5 +24,9 @@ export class GraphEdge {
 
     get destination(): GraphVertex {
         return this._destination;
+    }
+
+    get id(): string {
+        return this._uuid;
     }
 }
--- src/GraphVertex.ts
+++ src/GraphVertex.ts
@@ -1,12 +1,15 @@
 import { GraphEdge } from './GraphEdge';
+import { v4 as uuid } from 'uuid';
 
 export class GraphVertex {
     private readonly _name: string;
     private readonly _edges: Array<GraphEdge>;
+    private readonly _uuid: string;
 
     constructor(name: string) {
         this._name = name;
         this._edges = [];
+        this._uuid = uuid();
     }
 
     get name(): string {
@@ -15,5 +18,9 @@ export class GraphVertex {
 
     get edges(): Array<GraphEdge> {
         return this._edges;
+    }
+
+    get id(): string {
+        return this._uuid;
     }
 }
